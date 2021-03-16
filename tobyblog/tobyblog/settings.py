9c425f7fc138da633198d586567d85033246738c
toby <itoby24@gmail.com>
2021-03-16 01:08:46 +0000

    Setting up for Support for Heroku

--- tobyblog/tobyblog/settings.py
+++ tobyblog/tobyblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +142,4 @@ LOGIN_URL = 'login'
 
 
 
+django_heroku.settings(locals())
