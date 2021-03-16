b297fff2a904957f666c00aa22657d41c76f21ff
toby <itoby24@gmail.com>
2021-03-16 02:37:22 +0000

    more changes

--- tobyblog/tobyblog/settings.py
+++ tobyblog/tobyblog/settings.py
@@ -58,9 +58,7 @@ ROOT_URLCONF = 'tobyblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
